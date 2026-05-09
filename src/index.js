3a0034284b30b7398bd033c7261a6cedf381d31f
Prince Yadav <prince1bot001@gmail.com>
2026-05-09 22:56:31 +0530

    initialized socket.io

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import 'dotenv/config';
+import http from 'http';
+import { Server } from 'socket.io';
 import express from 'express';
 import {connectPG} from './database/pg.js';
 import { configDotenv } from 'dotenv';
@@ -9,18 +11,26 @@ import messageRouter from './routes/messageRoutes.js';
 import connectMongo from './database/mongo.js';
 
 const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server);
+
 app.use(express.json());
 app.use(cookieParser());
 
+io.on("connection", (socket) => {
+  console.log("connection established");
+})
+
 app.use('/auth', authRouter);
 app.use('/users', userRouter);
-app.use('/chat', messageRouter);
+app.use('/chat', messageRouter); 
 
 const startServer = async () => {
   try {
     await connectPG();
     await connectMongo();
-    app.listen(process.env.PORT, () => {
+    server.listen(process.env.PORT, () => {
       console.log(`Server is running on port ${process.env.PORT}...`);
     });
   } catch (err) {
